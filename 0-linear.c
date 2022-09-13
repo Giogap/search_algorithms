0855f6c58b53000e3c608b772763ddfb3759ba3a
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-12 23:16:55 -0500

    update 0

--- 0-linear.c
+++ 0-linear.c
@@ -1,5 +1,12 @@
+#include "search_algos.h"
 
-
+/**
+ * linear_search - check code
+ * @array: array
+ * @size: size to array
+ * @value: key
+ * Return: always -1
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
